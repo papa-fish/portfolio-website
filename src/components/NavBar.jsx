1dc0b2ad16fb116616e86d889c3d1be8e58ce289
Luke Moore <Lukechristophermoore@gmail.com>
2023-08-22 08:57:04 +1000

    menu for mobile and refactor mobile responsiveness

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useRef, useState } from 'react';
 
 import logo from '../logo.svg';
 import './NavBar.css'
 
 export default function NavBar() {
+
+    const menuOverlayRef = useRef(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const exitMenu = () => {
+        setIsMenuOpen(false);
+        menuOverlayRef.current.style.left = "-100%";
+    };
+    
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        const menuOverlay = menuOverlayRef.current;
+        menuOverlay.style.left = menuOverlay.style.left === "0%" ? "-100%" : "0%";
+    };
 
     return(
         <div className='nav'>
@@ -13,7 +28,7 @@ export default function NavBar() {
                         <img className='logo' src={logo} alt='logo' />
                     </Link>
                 </div>
-                <ul>
+                <ul className='desktop-nav'>
                     <Link to='/projects' className='projects-link'>
                         <li>Projects</li>
                     </Link>
@@ -32,6 +47,47 @@ export default function NavBar() {
                         <li>GitHub</li>
                     </a>
                 </ul>
+
+                <div 
+                    id="menuOverlay" 
+                    className={isMenuOpen ? "overlay open" : "overlay"} 
+                    ref={menuOverlayRef}>
+                    <button className="exit-button" id="exitButton" onClick={exitMenu}>✕</button>
+
+                    <div className="menu-item">
+                        <Link 
+                            to='/projects' 
+                            className='projects-link'
+                            onClick={exitMenu}
+                            >Projects
+                        </Link>
+                    </div>
+
+                    <div className="menu-item">
+                        <a 
+                            href="mailto:lukechristophermoore@gmail.com?subject=Hi Luke!" 
+                            target="_blank" 
+                            rel='noreferrer' 
+                            className='projects-link'
+                            >Contact
+                        </a>
+                    </div>
+
+                    <div className="menu-item">
+                        <a 
+                            href="https://github.com/papa-fish" 
+                            target='_blank' 
+                            rel='noreferrer'
+                            className='projects-link'
+                            >GitHub
+                        </a>
+                    </div>
+                </div>
+                <button className="mobile-nav" id="navToggle" onClick={toggleMenu}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </button>
             </nav>
             <hr className='navbar-bottom-line'/>
         </div>
